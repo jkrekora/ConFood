c465b43651277793f63036be8dc7cdaa67e7e195
Jakub Krekora <jakub_krekora@wp.pl>
2017-05-17 20:11:53 +0200

    capture photo added test1

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,62 @@
+var pictureSource;   
+var destinationType;
+document.addEventListener("deviceready",onDeviceReady,false);
+
+function onDeviceReady() {
+   pictureSource=navigator.camera.PictureSourceType;
+   destinationType=navigator.camera.DestinationType;
+}
+
+function onPhotoDataSuccess(imageURI) {
+
+   var smallImage = document.getElementById('smallImage');
+   smallImage.style.display = 'block';
+   smallImage.src = imageURI;
+   movePic(imageURI);
+}
+
+function capturePhoto() {
+   navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+   destinationType: destinationType.FILE_URI,
+   saveToPhotoAlbum: true});
+}
+
+function onFail(message) {
+   alert('Failed because: ' + message);
+}
+
+function movePic(file){
+   window.resolveLocalFileSystemURI(file, resolveOnSuccess, resOnError);
+}
+
+function resolveOnSuccess(entry){
+   var d = new Date();
+   var n = d.getTime();
+
+   var newFileName = n + ".jpg";
+   var myFolderApp = "ConFood";
+
+   window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSys)  {  
+        //The folder is created if doesn't exist
+    var direct = fileSys.root;
+          direct.getDirectory( myFolderApp,
+            {create:true, exclusive: false},
+            function(myFolderApp) {
+                entry.moveTo(myFolderApp, newFileName,  successMove,  resOnError);
+            },
+            resOnError);
+    },
+    resOnError);
+}
+
+function successMove(entry) {
+   sessionStorage.setItem('imagepath', entry.fullPath);
+
+}
+
+function resOnError(error) {
+   alert(error.code); 
+
  function getImage() {
  navigator.camera.getPicture(uploadPhoto, function(message) {
  alert('get picture failed');
