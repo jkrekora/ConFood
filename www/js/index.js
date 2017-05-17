f352ed67e338f4a073b6d23fb9f82870aada60da
Jakub Krekora <jakub_krekora@wp.pl>
2017-05-17 19:11:56 +0200

    new server for upload test4

--- www/js/index.js
+++ www/js/index.js
@@ -1,48 +1,30 @@
-// Wait for PhoneGap to load
-document.addEventListener("deviceready", onDeviceReady, false);
-
-// PhoneGap is ready
-function onDeviceReady() {
-// Do cool things here...
-}
-
-function getImage() {
-	// Retrieve image file location from specified source
-	navigator.camera.getPicture(uploadPhoto, function(message) {
-	alert('get picture failed');
-},{
-	quality: 50, 
-	destinationType: navigator.camera.DestinationType.FILE_URI,
-	sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
-}
-	);
-
+ function getImage() {
+ navigator.camera.getPicture(uploadPhoto, function(message) {
+ alert('get picture failed');
+ }, {
+ quality: 100,
+ destinationType: navigator.camera.DestinationType.FILE_URI,
+ sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+ });
 }
 
 function uploadPhoto(imageURI) {
-	var options = new FileUploadOptions();
-	options.fileKey="file";
-	options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
-	options.mimeType="image/jpeg";
+ var options = new FileUploadOptions();
+ options.fileKey = "file";
+ options.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
+ options.mimeType = "image/jpeg";
+ console.log(options.fileName);
+ var params = new Object();
+ params.value1 = "test";
+ params.value2 = "param";
+ options.params = params;
+ options.chunkedMode = false;
 
-	var params = new Object();
-	params.value1 = "test";
-	params.value2 = "param";
-
-	options.params = params;
-	options.chunkedMode = false;
-
-	var ft = new FileTransfer();
-	ft.upload(imageURI, "http://jkrekora.cba.pl/upload.php", win, fail, options);
-}
-
-function win(r) {
-	console.log("Code = " + r.responseCode);
-	console.log("Response = " + r.response);
-	console.log("Sent = " + r.bytesSent);
-	alert(r.response);
-}
-
-function fail(error) {
-	alert("An error has occurred: Code = " = error.code);
-}
+var ft = new FileTransfer();
+ ft.upload(imageURI, "http://jkrekora.cba.pl/upload.php", function(result){
+ console.log(JSON.stringify(result));
+ }, function(error){
+ console.log(JSON.stringify(error));
+ }, options);
+ }
+ </script>
