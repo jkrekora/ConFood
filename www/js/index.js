35c8e4c5142a4ade936e870f8c48c6fa72857cc7
Jakub Krekora <jakub_krekora@wp.pl>
2017-08-18 18:03:57 +0200

    index.html, index.js modified, test1

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,5 @@
 function showProducts(){
-	var productList = document.getElementById("product-list");
+    var productList = document.getElementById("product-list");
 	selectedProducts = productList.getElementsByTagName("input");
 	for(i = 0; i < selectedProducts.length; i++){
 		if(selectedProducts[i].checked){
@@ -8,117 +8,107 @@ function showProducts(){
 	}
 }
 function getSelectedRecipes(){
-	    if (selectedProducts==null) {
-        document.getElementById("recipe-list").innerHTML = "Powietrzem się nie najesz! Cofnij i wybierz produkty, które masz"; 
-		document.getElementById("optional-image").innerHTML = "<img src='img/talerz_pusty.jpg'/>";
+    if (selectedProducts==null){
+        document.getElementById("recipe-list").innerHTML = "Powietrzem się nie najesz! Cofnij i wybierz produkty, które masz";
+        document.getElementById("optional-image").innerHTML = "<img src='img/talerz_pusty.jpg'/>";
         return;
-    } else { 
-        if (window.XMLHttpRequest) {
+    } else{
+        if (window.XMLHttpRequest){
             xmlhttp = new XMLHttpRequest();
-        } else {
+        } else{
             xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
+        xmlhttp.onreadystatechange = function(){
+            if (this.readyState == 4 && this.status == 200){
                 document.getElementById("txtHint").innerHTML = this.responseText;
             }
         };
-        xmlhttp.open("GET","http://jkrekora.cba.pl/db.php?q=",true);
+        xmlhttp.open("GET","http://jkrekora.cba.pl/db.php?q=", true);
         xmlhttp.send();
     }
 }
-function showMeal() {
-        if (window.XMLHttpRequest) {
-            xmlhttp = new XMLHttpRequest();
-        } else {
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+function showMeal(){
+    if (window.XMLHttpRequest){
+        xmlhttp = new XMLHttpRequest();
+    } else{
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200){
+            document.getElementById("txtHint").innerHTML = this.responseText;
         }
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("txtHint").innerHTML = this.responseText;
-            }
-        xmlhttp.open("GET","http://jkrekora.cba.pl/db.php?q=str"true);
+        xmlhttp.open("GET","http://jkrekora.cba.pl/db.php?q=str",true);
         xmlhttp.send();
     }
 }
-
 var pictureSource;   
 var destinationType;
 var selectedProducts=null;
 document.addEventListener("deviceready",onDeviceReady,false);
-
-function onDeviceReady() {
-   pictureSource=navigator.camera.PictureSourceType;
-   destinationType=navigator.camera.DestinationType;
+function onDeviceReady(){
+    pictureSource=navigator.camera.PictureSourceType;
+    destinationType=navigator.camera.DestinationType;
 }
-function onPhotoDataSuccess(imageURI) {
-
-   var smallImage = document.getElementById('smallImage');
-   smallImage.style.display = 'block';
-   smallImage.src = imageURI;
-   movePic(imageURI);
+function onPhotoDataSuccess(imageURI){
+    var smallImage = document.getElementById('smallImage');
+    smallImage.style.display = 'block';
+    smallImage.src = imageURI;
+    movePic(imageURI);
 }
-function capturePhoto() {
-   navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-   destinationType: destinationType.FILE_URI,
-   saveToPhotoAlbum: true});
+function capturePhoto(){
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+        destinationType: destinationType.FILE_URI,
+        saveToPhotoAlbum: true});
 }
-function onFail(message) {
-   alert('Failed because: ' + message);
+function onFail(message){
+    alert('Failed because: ' + message);
 }
 function movePic(file){
-   window.resolveLocalFileSystemURI(file, resolveOnSuccess, resOnError);
+    window.resolveLocalFileSystemURI(file, resolveOnSuccess, resOnError);
 }
 function resolveOnSuccess(entry){
-   var d = new Date();
-   var n = d.getTime();
-
-   var newFileName = n + ".jpg";
-   var myFolderApp = "ConFood";
-
-   window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSys)  {  
-        //The folder is created if doesn't exist
-    var direct = fileSys.root;
-          direct.getDirectory( myFolderApp,
-            {create:true, exclusive: false},
-            function(myFolderApp) {
-                entry.moveTo(myFolderApp, newFileName,  successMove,  resOnError);
-            },
-            resOnError);
-    },
-    resOnError);
+    var d = new Date();
+    var n = d.getTime();var newFileName = n + ".jpg";
+    var myFolderApp = "ConFood";
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSys) {
+            var direct = fileSys.root;
+            direct.getDirectory( myFolderApp,{
+                    create:true, exclusive: false},
+                function(myFolderApp){
+                    entry.moveTo(myFolderApp, newFileName,  successMove,  resOnError);
+                    }, resOnError);
+    }, resOnError);
 }
-function successMove(entry) {
-   sessionStorage.setItem('imagepath', entry.fullPath);
+function successMove(entry){
+    sessionStorage.setItem('imagepath', entry.fullPath);
 }
-function resOnError(error) {
-   alert(error.code);
+function resOnError(error){
+    alert(error.code);
 }
- function getImage() {
- navigator.camera.getPicture(uploadPhoto, function(message) {
- alert('get picture failed');
- }, {
- quality: 100,
- destinationType: navigator.camera.DestinationType.FILE_URI,
- sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
- });
+function getImage(){
+    navigator.camera.getPicture(uploadPhoto, function(message){
+        alert('get picture failed');
+    }, {
+        quality: 100,
+        destinationType: navigator.camera.DestinationType.FILE_URI,
+        sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+    });
 }
-function uploadPhoto(imageURI) {
- var options = new FileUploadOptions();
- options.fileKey = "file";
- options.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
- options.mimeType = "image/jpeg";
- console.log(options.fileName);
- var params = new Object();
- params.value1 = "test";
- params.value2 = "param";
- options.params = params;
- options.chunkedMode = false;
-
-var ft = new FileTransfer();
- ft.upload(imageURI, "http://jkrekora.cba.pl/upload.php", function(result){
- console.log(JSON.stringify(result));
+function uploadPhoto(imageURI){
+    var options = new FileUploadOptions();
+    options.fileKey = "file";
+    options.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
+    options.mimeType = "image/jpeg";
+    console.log(options.fileName);
+    var params = new Object();
+    params.value1 = "test";
+    params.value2 = "param";
+    options.params = params;
+    options.chunkedMode = false;
+    var ft = new FileTransfer();
+    ft.upload(imageURI, "http://jkrekora.cba.pl/upload.php", function(result){
+        console.log(JSON.stringify(result));
  }, function(error){
- console.log(JSON.stringify(error));
+        console.log(JSON.stringify(error));
  }, options);
  }
